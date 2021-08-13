1bc2294a909c267cc7cf667f518b245679d39967
hansol <kimhan0421@naver.com>
2021-08-13 21:49:40 +0900

    Fix heroku setting

--- django_/myproject/myproject/settings.py
+++ django_/myproject/myproject/settings.py
@@ -33,7 +33,7 @@ DATABASES = my_settings.DATABASES
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
